caca3d556d90dbe7ae7bb36ccebae141333af658
Amrita <amritakumari13022006@gamil.com>
2025-07-02 16:28:41 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+"use strict" ; // treat all JS code as newer version
+
+//alert(3+3) error as we are using node js and not browser
+
+//code readability should be high.
+
+console.log(3+3);
+console.log("Hello world")
+
+let name = "Amrita"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stand alone value.(reperesentation of empty value)
+//undefined => value not assigned
+//symbol => unique
+//object
+
+console.log(typeof "Amrita");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+

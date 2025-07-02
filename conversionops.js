1b626f7fc2f9c6de1655b22209df0bdcbfcc0082
Amrita <amritakumari13022006@gamil.com>
2025-07-02 20:29:28 +0530

    operations

--- conversionops.js
+++ conversionops.js
@@ -21,5 +21,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//********************OPERATIONS********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1= "hello "
+let str2= "World"
+
+let str3= str1+ str2
+// console.log(str3)
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+//if string is first,  the whole  equation is treated as a string
+
+
+
